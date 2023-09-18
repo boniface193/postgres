969ff428c0a240f0935db6caa343ebbc4c6c006c
Ikechukwu <bonifacedennis193@gmail.com>
2023-09-18 18:49:14 +0100

    completed sql project

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- Database: vet_clinic
+
+-- DROP DATABASE IF EXISTS vet_clinic;
+
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Nigeria.1252'
+    LC_CTYPE = 'English_Nigeria.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
