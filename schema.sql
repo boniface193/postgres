d7a8c9c4e2e829054a2b6c597611fe2595c6f37e
Ikechukwu <bonifacedennis193@gmail.com>
2023-09-20 08:47:06 +0100

    updated schema

--- schema.sql
+++ schema.sql
@@ -4,12 +4,4 @@
 
 -- DROP DATABASE IF EXISTS vet_clinic;
 
-CREATE DATABASE vet_clinic
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_Nigeria.1252'
-    LC_CTYPE = 'English_Nigeria.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+CREATE TABLE animals(id INT PRIMARY NOT NULL, name VARCHAR(255), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(4,0));
