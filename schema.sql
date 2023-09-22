6a2986ae184f163ae717f54d3695b3f59a66946a
Ikechukwu <bonifacedennis193@gmail.com>
2023-09-22 13:36:02 +0100

    updated changes

--- schema.sql
+++ schema.sql
@@ -7,4 +7,4 @@
 CREATE TABLE animals(id INT PRIMARY NOT NULL, name VARCHAR(255), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(4,0));
 CREATE TABLE owners (id serial PRIMARY KEY, full_name varchar(255), age integer);
 CREATE TABLE species (id serial PRIMARY KEY, name varchar(255));
-ALTER TABLE animals ADD COLUMN species_id integer, ADD COLUMN owner_id integer, ADD PRIMARY KEY (id), ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id), ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id);
+ALTER TABLE animals DROP COLUMN species, ADD COLUMN species_id integer, ADD COLUMN owner_id integer, ADD PRIMARY KEY (id), ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id), ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id);
